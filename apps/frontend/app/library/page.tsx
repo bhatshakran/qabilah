c283215ca3d1372218e080a3d31afa23e112c467
Shaqran Bhat <bhatshaqran@protonmail.com>
2026-02-10 23:15:30 +0530

    feat: :sparkles: fetch vocab on server.

--- apps/frontend/app/library/page.tsx
+++ apps/frontend/app/library/page.tsx
@@ -1,15 +1,28 @@
+import { SWRConfig } from "swr";
 import Header from "../_components/header";
 import FlashcardApp from "../flashcard";
 
-export default function Library() {
+export default async function Library() {
+  const vocab = await fetch(`${process.env.BASE_URL}/api/vocabulary`, {
+    cache: "no-store",
+  }).then((res) => {
+    return res.json();
+  });
+
   return (
-    <>
+    <SWRConfig
+      value={{
+        fallback: {
+          "/api/vocabulary": vocab,
+        },
+      }}
+    >
       <Header />
       <div className="flex justify-center bg-zinc-950 bg-[radial-gradient(circle_at_center,var(--tw-gradient-stops))] from-zinc-900 via-zinc-950 to-black">
         <main className="w-full flex justify-center items-start flex-col p-4 md:max-w-2xl">
           <FlashcardApp />
         </main>
       </div>
-    </>
+    </SWRConfig>
   );
 }
